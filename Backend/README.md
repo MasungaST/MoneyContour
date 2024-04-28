493f72343a535f8247d48be78e9f7135842a90cb
Stephen Masunga <steven.masunga@gmail.com>
2024-04-28 16:17:30 +0200

    Express.ts backend successfully connected to the PostgreSQL database

---
+++ Backend/README.md
@@ -0,0 +1,9 @@
+# MoneyContour Backend server
+
+This is the MoneyContour backend server project build on Node.js.
+It is an Express.js projetc written in TypeScript
+
+It is a Restfull Server with GraphQl incoperrated to manage the request, Sequelize to communicate with the Database and Apollo Server.
+
+To run the project for the first time, run
+##npm i## to install packages, then ##npm run build## to build the project, then finally ##npm run dev## to ru the project.
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,14 +1,27 @@
 import express, { Express, Request, Response } from"express";
 import dotenv from "dotenv";
+import pool from "./db";
+import bodyParser = require("body-parser");
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("MoneyContour Server");
 });
+
+//Test the database connection
+pool.connect((err, client, done) => {
+    if (err) {
+        console.error('Error connection to the MoneyContour database:', err);
+    } else {
+        console.log("Connected to the MoneyContour database");
+    }
+})
 
 app.listen(port, () => {
     console.log(`[server]: running at https://localhost:${port}`);
