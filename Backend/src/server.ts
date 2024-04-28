e030a62e24b5339ed967f954e605865213d9d998
Stephen Masunga <steven.masunga@gmail.com>
2024-04-28 12:02:27 +0200

    Backend initial setup complete, app running great

---
+++ Backend/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from"express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("MoneyContour Server");
+});
+
+app.listen(port, () => {
+    console.log(`[server]: running at https://localhost:${port}`);
+})
+
+
