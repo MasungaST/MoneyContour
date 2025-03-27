d1514f18028066a490d540657c5efe45ec1a5fde
MasungaST <you@example.com>
2025-03-27 22:51:22 +0200

    Documents updated: Insert sql file for adding profile entities created

---
+++ Documentation/Database/SQL scripts/Insert_delete data/Insert_master.sql
@@ -0,0 +1,6 @@
+INSERT INTO public."Profile" (
+    "Name", "Surname", "DateOfBirth", "Gender", "Email", "Password"
+) 
+		VALUES (
+		    'Stephen', 'Masunga', '1996-03-25 02:39:07.053', 'Male', 'steven.masunga@gmail.com', 'master'
+		);
